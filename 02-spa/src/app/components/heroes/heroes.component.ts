c9dc8f2322e9187603a84b1e78ca524abbadedd2
foxcarlos <foxcarlos@gmail.com>
2018-09-27 11:03:44 -0300

    Creando un servicio

--- 02-spa/src/app/components/heroes/heroes.component.ts
+++ 02-spa/src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../servicios/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
+  heroes: any[] = [];
 
-  constructor() { }
+  constructor(private _heroesService: HeroesService) {
+  }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
