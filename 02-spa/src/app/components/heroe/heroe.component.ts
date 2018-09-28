6b5247a3ad9e79f482d2ba98df2f0564c0a9961c
foxcarlos <foxcarlos@gmail.com>
2018-09-28 13:08:30 -0300

    tarea terminada

--- 02-spa/src/app/components/heroe/heroe.component.ts
+++ 02-spa/src/app/components/heroe/heroe.component.ts
@@ -10,24 +10,13 @@ import { HeroesService } from '../servicios/heroes.service';
 export class HeroeComponent {
 
   heroe: any = {};
-  marvel = false;
-  dc = false;
 
   constructor( private _activateRoute: ActivatedRoute,
                private _heroesService: HeroesService) {
 
     this._activateRoute.params.subscribe( params => {
       this.heroe = this._heroesService.getHeroe(params['id']);
-      if (this.heroe.casa = 'Marvel') {
-        this.marvel = true;
-        this.dc = false;
-
-      } else {
-        this.dc = true;
-        this.marvel = false;
-      }
       console.log(this.heroe.casa);
     });
   }
 }
-
