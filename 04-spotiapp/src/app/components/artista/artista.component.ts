b2d3c697d92dc734c5cff535cc9f1d798e918bfd
foxcarlos <foxcarlos@gmail.com>
2018-10-19 12:19:35 -0300

    creacion de la pagina del artista

--- 04-spotiapp/src/app/components/artista/artista.component.ts
+++ 04-spotiapp/src/app/components/artista/artista.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SpotifyService } from '../../services/spotify.service';
 
 
 @Component({
@@ -9,10 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ArtistaComponent {
 
-  constructor(private router: ActivatedRoute) {
+  artista: any = {};
+  loading: boolean;
+
+  constructor(private router: ActivatedRoute, private spotify: SpotifyService) {
+    this.loading = true;
     router.params.subscribe(params => {
-      console.log(params);
+      this.getArtista(params.id);
     });
+  }
+
+  private getArtista( id: string) {
+    this.loading = true;
+
+    this.spotify.getArtista( id )
+      .subscribe( (artista: any) => {
+        console.log( artista );
+        this.artista = artista;
+        this.loading = false;
+      });
   }
 
 }
--- 04-spotiapp/src/app/services/spotify.service.ts
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -16,7 +16,7 @@ export class SpotifyService {
      const url = `https://api.spotify.com/v1/${query} `;
 
      const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQCgL6iySc0RDT8l8tT8RbDE4-hbXrK-vKYppA-jUTnVbj9GocWgSjII6SwkOAdnj7f-zsnMVXSIJOzfnOpv5K-k-A5aCxXOfqrzqs4G41ZWakLlfNbkixY8YDXGrHoqP1zROQzp--uSsTmq'
+      'Authorization': 'Bearer BQA0hEN2Ln2THtEBMSnWl96u12z0QvsjKP7LQ6iG-aS-52esi-KyBOxa8EufcCYrIBVOhbs6dtTcH8tFHdHW582RJ_RM9gmhyDlBHMFv-Q4_zFM6bDbDIH1WJq6lFLADhIXaYuGaP0xInXh8'
       });
 
      return this.http.get(url, {headers});
@@ -29,10 +29,14 @@ export class SpotifyService {
          }));
   }
 
-  getArtista(termino: string) {
+  getArtistas(termino: string) {
     return this.getQuery(`search?query=${ termino }&type=artist&market=AR&offset=0&limit=15`)
         .pipe( map( data => {
           return data['artists'].items;
         }));
+  }
+
+  getArtista(id: string) {
+    return this.getQuery(`artists/${id}`);
   }
 }
